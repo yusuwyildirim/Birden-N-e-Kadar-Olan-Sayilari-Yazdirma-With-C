2fbf51aef59c24e0c6da81d46df2c6bcc29ab0a4
Yusuf Yıldırım <baba_yusuf2324@hotmail.com>
2022-10-28 16:09:05 +0300

    Birden N'e Kadar Olan Sayıları Yazdırma Witch C
    C dili ile birden N'e kadar olan sayıları yazdırma programı

---
+++ Birden N'e Kadar Olan Sayıları Yazdırma/Birden N'e Kadar Olan Sayıları Yazdırma.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	int x,i,sonuc;
+	
+	printf("kaca kadar yazdirmak istiyorsunuz:\n");
+	scanf("%d",&x);
+	printf("\n\n\n");
+	
+	for(i=1;i<=x;i++)
+	{
+		printf("%d\n",i);
+	}
+	
+	
+	
+	
+	
+	
+	getch(0);
+	return 0;
+}
